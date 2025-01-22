e6208c84ac12af2339dc6267e308e3257d45ea33
RIANE-GAMBOA <rianegamboa4441@gmail.com>
2025-01-22 19:50:29 +0800

    Linking Database to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
 import Link from "next/link";
 import {db} from "~/server/db"
 
-const mockUrls = [
-  "https://ezlhdp1z8l.ufs.sh/f/rVNTt4c6mQKZYrgI7ft4iMe7rGOpU9WXkozH8P0mLDtKByCS",
-  "https://ezlhdp1z8l.ufs.sh/f/rVNTt4c6mQKZ01LkJXUqQ9nFr2GtNR8i6WHhdEo0SVXmkblz",
-  "https://ezlhdp1z8l.ufs.sh/f/rVNTt4c6mQKZhfytW9G3O1sNARPiyT8bQrl7emB2ZELMjIU9",
-]
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}))
+export const dynamic = "force-dynamic"
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const images = await db.query.images.findMany({
+    orderBy: (module, {desc}) => desc(module.id)
+  });
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div key={image.id} className="w-48 p-1">
             <img
-              src={image.url} className="w-full h-full object-cover rounded-md"
+              src={image.url} className="w-ful l h-full object-cover rounded-md"
             />
+            <div> {image.name}</div>
           </div>
         ))}
       </div>
